1ca22661932d036ae199d99f81f36279be11b373
William Travis Holton <heytrav@cpan.org>
2014-06-27 12:11:17 +0000

    Import of HEYTRAV/Raygun4perl-0.004 from CPAN.
    gitpan-cpan-distribution: Raygun4perl
    gitpan-cpan-version:      0.004
    gitpan-cpan-path:         HEYTRAV/Raygun4perl-0.004.tar.gz
    gitpan-cpan-author:       HEYTRAV
    gitpan-cpan-maturity:     released

--- lib/Raygun4perl.pm
+++ lib/Raygun4perl.pm
@@ -1,6 +1,5 @@
 package Raygun4perl;
 
-use Mouse;
 
 =head1 NAME
 
@@ -8,30 +7,207 @@ Raygun4perl - Connect to the Raygun.io API
 
 =head1 SYNOPSIS
 
-  use Raygun4perl;
+  use Raygun4perl::Message;
+  use Raygun4perl::Messenger;
 
-  # synopsis...
+    my $message = Raygun4perl::Message->new(
+        user => 'null@null.com',
+        client => {
+            name      => 'something',
+            version   => 2,
+            clientUrl => 'www.null.com'
+        },
+        error       => {
+            stack_trace => [ { line_number => 34 } ]
+        },
+        environment => {
+            processor_count       => 2,
+            cpu                   => 34,
+            architecture          => 'x84',
+            total_physical_memory => 3
+        },
+        request => HTTP::Request->new(
+            POST => 'https://www.null.com',
+            [ 'Content-Type' => 'text/html', ]
+        ),
+
+    );
+
+    my $message = $message->arm_the_laser;
+    my $raygun = Raygun4perl::Messenger->new( api_key => '<your raygun.io api key>' );
+    my $response = $raygun->fire_the_laser($message);
 
 =head1 DESCRIPTION
 
-# longer description...
-
-
-=head1 INTERFACE
-
-=cut
-
-
-
+Interface for the Raygun.io API.
 
 
 =head1 DEPENDENCIES
 
+You will need to have an API key from raygun.io. By default, this module
+checks C<$ENV{RAYGUN_API_KEY}>.
+
+=over 2
+
+=item DateTime
+
+=back
+
+=over 2
+
+=item DateTime::Format::Strptime
+
+=back
+
+=over 2
+
+=item Filesys::DfPortable 
+
+=back
+
+=over 2
+
+=item HTTP::Request 
+
+=back
+
+=over 2
+
+=item JSON 
+
+=back
+
+=over 2
+
+=item LWP::UserAgent 
+
+=back
+
+
+=over 2
+
+=item Mouse 
+
+=back
+
+=over 2
+
+=item Mouse::Util::TypeConstraints 
+
+=back
+
+=over 2
+
+=item Mozilla::CA 
+
+=back
+
+=over 2
+
+=item POSIX 
+
+=back
+
+=over 2
+
+=item Sys::Info 
+
+=back
+
+=over 2
+
+=item Sys::Info::OS 
+
+=back
+
+=over 2
+
+=item URI
+
+=back
+
+=over 2
+
+=item Test::Class 
+
+=back
+
+=over 2
+
+=item Test::Deep 
+
+=back
+
+=over 2
+
+=item Test::Exception 
+
+=back
+
+=over 2
+
+=item Test::More 
+
+=back
+
+=over 2
+
+=item parent 
+
+=back
+
+=over 2
+
+=item strict 
+
+=back
+
+=over 2
+
+=item warnings
+
+=back
+
 
 =head1 SEE ALSO
 
-=cut
+=over 2
 
+=item L<Raygun4perl::Messenger|Raygun4perl::Messenger>
+
+=back
+
+=over 2
+
+=item L<Raygun4perl::Message|Raygun4perl::Message>
+
+=back
+
+=over 2
+
+=item L<Raygun4perl::Message::Request|Raygun4perl::Message::Request>
+
+=back
+
+=over 2
+
+=item L<Raygun4perl::Message::Environment|Raygun4perl::Message::Environment>
+
+=back
+
+=over 2
+
+=item L<Raygun4perl::Message::Error|Raygun4perl::Message::Error>
+
+=back
+
+=over 2
+
+=item L<Raygun4perl::Message::Error::StackTrace|Raygun4perl::Message::Error::StackTrace>
+
+=back
+
+=cut
 
 1;
 
